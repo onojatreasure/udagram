e32539b085d16c252c4f8ee69ceace3844cdfd6d
Treasure Onoja <treasureonoja@Treasures-MacBook-Air.local>
2021-09-24 16:21:24 +0100

    first commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgresadmin
+export POSTGRES_HOST=treasure-project3-cdr.crlcjqjaittw.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::treasure-udacity-project3
+export AWS_REGION=us-west-2
+#export AWS_PROFILE=treasure
+#export JWT_SECRET=testing
+#export URL=http://localhost:8100
